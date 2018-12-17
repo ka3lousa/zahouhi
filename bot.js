d3bcacf6d5eafa6a8d1e9662b5e3cb2c9f4ec315
ka3lousa <45945493+ka3lousa@users.noreply.github.com>
2018-12-17 17:10:09 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("523808032921092116")
+setInterval(function() {
+channel.send(`hi  i am the best in this houma i am boyka kabir enta kabir haywan kalb`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
